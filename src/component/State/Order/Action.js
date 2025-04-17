0a3a0fc3b76a12069b0e549ce984ec48c0ff2fb8
harshk1010 <113081552+harshk1010@users.noreply.github.com>
2025-04-17 23:22:17 +0530

    Update Action.js

--- src/component/State/Order/Action.js
+++ src/component/State/Order/Action.js
@@ -13,6 +13,8 @@ export const createOrder = (reqData) => {
             });
             if(data.payment_url) {
                 window.location.href=data.payment_url;
+            } else {
+                console.log("No payment_url returned from backend");
             }
             console.log("created order data ",data);
             dispatch({type: CREATE_ORDER_SUCCESS, payload: data});
